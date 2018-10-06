220ef8cc968a1df2f012f87a21c8c6f2a70f36e8
LAPTOP-15V47R3F\Usuario <leandrojesusljdmdq@gmail.com>
2018-10-05 21:02:43 -0300

    Entrega Tp5

---
+++ Dall'Armellina,Leandro/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <formulario @agregar="agregar"></formulario>
+    <lista :personas="personas"></lista>            
+  </div>   
+</template>
+
+<script>
+import formulario from './components/formulario.vue';
+import lista from './components/lista.vue';
+
+
+export default {
+	name: 'app',
+	components: {
+    formulario,
+    lista
+  },
+	data() {
+		return {
+			personas: [],
+      persona: 
+      {
+        nombre: "",
+        edad: "",
+        sexo: ""
+      }
+		}
+	},
+	methods: {
+    agregar(nombre,edad,sexo) {
+      this.personas.push({
+        nombre: nombre,
+        edad: edad,
+        sexo: sexo
+      });
+    }
+	}
+}
+</script>
